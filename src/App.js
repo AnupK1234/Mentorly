89098290628c301a0b7b4915da651e0fc9264029
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-07-22 20:12:47 +0530

    updated path

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./components/home";
-import Navbar from "./components/navbar";
-import Footer from "./components/footer";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
