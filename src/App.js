a827bc49e734fade0c4a69e867092d0d043712b4
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-07-24 01:50:36 +0530

    mentee

--- src/App.js
+++ src/App.js
@@ -16,6 +16,14 @@ import MentorshipHistory from './components/mentor/MentorshipHistory';
 import MentorshipProgressTracking from './components/mentor/MentorshipProgressTracking';
 import Notifications from './components/mentor/Notifications';
 import MentorshipTipsAndResources from './components/mentor/MentorshipTipsAndResources';
+import MenteeFeedback from './components/mentee/FeedbackAndRatings';
+import MentorshipResources from './components/mentee/MentorshipResources';
+import SendRequest from './components/mentee/SendRequest';
+import ViewMentorshipHistory from './components/mentee/ViewMentorship';
+import UpcomingSessions from "./components/mentee/UpcomingSessions";
+import MenteeCalendarIntegration from './components/mentee/CalendarIntegration';
+import MenteeNotifications from './components/mentee/Notifications';
+
 
 function App() {
   return (
@@ -38,6 +46,13 @@ function App() {
           <Route path="/calendar_integration" element={<CalendarIntegration/>} exact/>
           <Route path="/mentor_settings" element={<MentorSettings/>} exact/>
           <Route path="/mentorship_tips_and_resources" element={<MentorshipTipsAndResources/>} exact/>
+          <Route path="/view_mentorship_history" element={<ViewMentorshipHistory/>} exact/>
+          <Route path="/view_upcoming_sessions" element={<UpcomingSessions/>} exact/>
+          <Route path="/mentee_give_feedback_and_ratings" element={<MenteeFeedback/>} exact/>
+          <Route path="/access_mentorship_resources" element={<MentorshipResources/>} exact/>
+          <Route path="/mentee_calendar_integration" element={<MenteeCalendarIntegration/>} exact/>
+          <Route path="/send_mentorship_requests" element={<SendRequest/>} exact/>
+          <Route path="/mentee_receive_notifications" element={<MenteeNotifications/>} exact/>
         </Routes>
       <Footer/>
       </BrowserRouter>
--- src/components/home.js
+++ src/components/home.js
@@ -1,33 +1,39 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
+import Logo from '../assets/mentorly.png';
 
 const Home = () => {
-  const { loginWithRedirect, isAuthenticated, user,logout } = useAuth0();
+  const { loginWithRedirect, isAuthenticated, user, logout } = useAuth0();
 
   return (
     <div className="bg-gradient-to-r from-green-400 to-lightGreen-500 p-3">
       <section className="py-16">
-        <div className="container mx-auto text-center">
-          <h1 className="text-4xl font-bold text-black">Welcome to Mentorly!</h1>
+        <div className="container mx-auto text-center flex items-center">
+          <div>
+          <img src={Logo} alt='logo' height={50} />
+          </div>
+         <div>
+         <h1 className="text-4xl font-bold text-black">Welcome to Mentorly!</h1>
           <p className="text-lg text-gray-800 mt-4">Connecting mentors and mentees to empower growth and learning.</p>
+         </div>
         </div>
       </section>
 
       <section className="py-12">
         <div className="container mx-auto">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-            <Link to="/user-profile" className="bg-white p-6 rounded-lg shadow hover:shadow-lg transition duration-300">
+            <Link to="/" className="bg-white p-6 rounded-lg shadow hover:shadow-lg transition duration-300">
               <h2 className="text-xl font-semibold">User Profiles</h2>
               <p className="mt-4">Create separate user profiles for both mentors and mentees to showcase their skills and interests.</p>
             </Link>
 
-            <Link to="/messaging" className="bg-white p-6 rounded-lg shadow hover:shadow-lg transition duration-300">
+            <Link to="/" className="bg-white p-6 rounded-lg shadow hover:shadow-lg transition duration-300">
               <h2 className="text-xl font-semibold">Messaging System</h2>
               <p className="mt-4">Facilitate communication between mentors and mentees with an integrated messaging system.</p>
             </Link>
 
-            <Link to="/rating-review" className="bg-white p-6 rounded-lg shadow hover:shadow-lg transition duration-300">
+            <Link to="/" className="bg-white p-6 rounded-lg shadow hover:shadow-lg transition duration-300">
               <h2 className="text-xl font-semibold">Rating & Review</h2>
               <p className="mt-4">Allow mentees to rate their mentors and provide valuable feedback through a review system.</p>
             </Link>
@@ -35,7 +41,7 @@ const Home = () => {
         </div>
       </section>
 
-       <section className="bg-lightGreen-100 py-12 text-center">
+      <section className="bg-lightGreen-100 py-12 text-center">
         <div className="container mx-auto">
           {isAuthenticated ? (
             <div>
