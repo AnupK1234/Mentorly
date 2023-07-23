55e85345d1f7584acda4093c274a7f29b54d253d
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-07-23 13:53:51 +0530

    app updated

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Navbar from "./components/navbar";
 import Footer from "./components/footer";
 import Blog from "./components/blog";
 import Event from "./components/event";
+import Goals from "./components/goals";
+import Mentor from "./components/mentor/Mentor";
+import Mentee from "./components/mentee/Mentee";
 
 function App() {
   return (
@@ -13,6 +16,9 @@ function App() {
           <Route path="/" element={<Home/>} exact/>
           <Route path="/blogs" element={<Blog/>} exact/>
           <Route path="/events" element={<Event/>} exact/>
+          <Route path="/goals" element={<Goals/>} exact/>
+          <Route path="/mentor" element={<Mentor/>} exact/>
+          <Route path="/mentee" element={<Mentee/>} exact/>
         </Routes>
       <Footer/>
       </BrowserRouter>
