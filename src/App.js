91fd6afa91a9fab176749bebfd9849ba94ea0283
Anup Khismatrao <khismatrao2003@gmail.com>
2023-07-23 11:44:56 +0530

    Added Events and Blogs Page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./components/home";
 import Navbar from "./components/navbar";
 import Footer from "./components/footer";
 import Blog from "./components/blog";
+import Event from "./components/event";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home/>} exact/>
           <Route path="/blogs" element={<Blog/>} exact/>
+          <Route path="/events" element={<Event/>} exact/>
         </Routes>
       <Footer/>
       </BrowserRouter>
