b550e2b12c1a2273c577b61371c221456927bd4b
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-07-24 00:16:19 +0530

    mentor code updated

--- src/App.js
+++ src/App.js
@@ -7,6 +7,15 @@ import Goals from "./components/goals";
 import Mentor from "./components/mentor/Mentor";
 import Mentee from "./components/mentee/Mentee";
 import Home from "./components/Home";
+import MentorshipRequests from './components/mentor/MentorshipRequest';
+import CalendarIntegration from './components/mentor/CalendarIntegration';
+import CreateNewMeeting from './components/mentor/CreateNewMeeting';
+import FeedbackAndRatings from './components/mentor/FeedbackAndRatings';
+import MentorSettings from './components/mentor/MentorSettings';
+import MentorshipHistory from './components/mentor/MentorshipHistory';
+import MentorshipProgressTracking from './components/mentor/MentorshipProgressTracking';
+import Notifications from './components/mentor/Notifications';
+import MentorshipTipsAndResources from './components/mentor/MentorshipTipsAndResources';
 
 function App() {
   return (
@@ -20,6 +29,15 @@ function App() {
           <Route path="/goals" element={<Goals/>} exact/>
           <Route path="/mentor" element={<Mentor/>} exact/>
           <Route path="/mentee" element={<Mentee/>} exact/>
+          <Route path="/mentorship_requests" element={<MentorshipRequests/>} exact/>
+          <Route path="/mentorship_history" element={<MentorshipHistory/>} exact/>
+          <Route path="/create_new_meeting" element={<CreateNewMeeting/>} exact/>
+          <Route path="/mentorship_progress_tracking" element={<MentorshipProgressTracking/>} exact/>
+          <Route path="/feedback_and_ratings" element={<FeedbackAndRatings/>} exact/>
+          <Route path="/notifications" element={<Notifications/>} exact/>
+          <Route path="/calendar_integration" element={<CalendarIntegration/>} exact/>
+          <Route path="/mentor_settings" element={<MentorSettings/>} exact/>
+          <Route path="/mentorship_tips_and_resources" element={<MentorshipTipsAndResources/>} exact/>
         </Routes>
       <Footer/>
       </BrowserRouter>
